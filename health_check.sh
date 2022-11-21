27d9a4d904763c13489ae1050154a4bae8c5df3b
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2022-11-21 21:31:25 +1300

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,18 @@
+
 #!/bin/sh
-if [ $(cat /tmp/check | grep -c G) ]; then
+if [ $(pidof nginx | wc -l) -eq 1 ]; then
   exit 0
 else
   exit 1
 fi
+
+# To test inside the pod
+
+# cat <<\EOF > test.sh
+# #!/bin/sh
+# if [ $(pidof nginx | wc -l) -eq 1 ]; then
+#   exit 0
+# else
+#   exit 1
+# fi
+# EOF
